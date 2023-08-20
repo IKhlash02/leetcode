8a683857fe0a5ee9a005fd8850e229a671c2c4af
IKhlash02 <119332007+IKhlash02@users.noreply.github.com>
2023-08-20 18:12:25 +0700

    Time: 254 ms (62.56%), Space: 143.9 MB (16.59%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.dart
+++ 0035-search-insert-position/0035-search-insert-position.dart
@@ -1,10 +1,19 @@
 class Solution {
   int searchInsert(List<int> nums, int target) {
-     for (var i = 0; i < nums.length; i++) {
-      if (target == nums[i]) return i;
+    int start = 0;
+    int end = nums.length - 1;
 
-      if (target < nums[i]) return i;
+    while (start <= end) {
+      int mid = start + (end - start) ~/ 2;
+      if (target == nums[mid]) {
+        return mid;
+      } else if (target > nums[mid]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
-    return nums.length;
+
+    return start;
   }
 }
