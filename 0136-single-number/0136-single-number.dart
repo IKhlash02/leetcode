5a6149d1c916a719adac2b81dcc19b2e05856b9b
IKhlash02 <119332007+IKhlash02@users.noreply.github.com>
2023-08-24 22:22:09 +0700

    Time: 273 ms (73.46%), Space: 150.6 MB (24.07%) - LeetHub

--- 0136-single-number/0136-single-number.dart
+++ 0136-single-number/0136-single-number.dart
@@ -2,9 +2,8 @@ class Solution {
   int singleNumber(List<int> nums) {
     Map<int, int> counterMap = {};
 
-    for (int i = 0; i < nums.length; i++) {
-   counterMap[nums[i]] = (counterMap[nums[i]] ?? 0) + 1;
-    }
+    for (int i = 0; i < nums.length; i++) counterMap[nums[i]] = (counterMap[nums[i]] ?? 0) + 1;
+    
 
     int ans = 0;
 
