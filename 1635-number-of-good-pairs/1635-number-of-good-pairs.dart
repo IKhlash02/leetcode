a872f8fe9de51420f374d1c474cab23bfcca214e
IKhlash02 <119332007+IKhlash02@users.noreply.github.com>
2023-08-29 19:56:08 +0700

    Time: 196 ms (94.44%), Space: 142.4 MB (35.18%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int numIdenticalPairs(List<int> nums) {
+      int n = nums.length;
+      int counter = 0;
+
+      for(int i = 0; i<n;i++)
+        for(int j = i+1;j<n;j++)
+            if(nums[i]==nums[j])
+                counter++;
+            
+    return counter;
+  }
+}
