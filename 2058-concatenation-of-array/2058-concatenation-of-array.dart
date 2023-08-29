888082001c3c8c22397da97fba9a16652825c708
IKhlash02 <119332007+IKhlash02@users.noreply.github.com>
2023-08-29 16:29:14 +0700

    Time: 229 ms (95.88%), Space: 145.2 MB (100%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.dart
+++ 2058-concatenation-of-array/2058-concatenation-of-array.dart
@@ -1,11 +1,12 @@
 class Solution {
   List<int> getConcatenation(List<int> nums) {
         int n = nums.length;
-      List<int> ans = List<int>.filled(n*2, 0);
+      List<int> ans = [];
       
-      for(int i = 0; i <n;i++){
-          ans[i] = nums[i];
-          ans[i+n] = nums[i];
+      for(int i = 0; i <n*2;i++){
+        int j = i%n;
+        ans.add(nums[j]);
+          
       }
 
       return ans;
