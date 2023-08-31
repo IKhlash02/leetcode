d22fa1db4b3dd2e86f2afed6fc52092497151505
IKhlash02 <119332007+IKhlash02@users.noreply.github.com>
2023-08-31 20:00:52 +0700

    Time: 57 ms (53.11%), Space: 43.1 MB (11.75%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let value = [];
+
+    for(let i = 0; i<nums.length; i++){
+        let sum = 0;
+        for(let j = 0; j<=i; j++){
+            sum += nums[j];
+        }
+        value.push(sum);
+    }
+
+    return value;
+
+};
