802241194843daf356f8e1d4edcc8fe64f196dba
IKhlash02 <119332007+IKhlash02@users.noreply.github.com>
2023-09-03 23:28:49 +0700

    Time: 52 ms (66.51%), Space: 41.9 MB (58.72%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
@@ -3,6 +3,6 @@
  * @return {string}
  */
 var interpret = function(command) {
-    let value = command.replaceAll("(al)", "al").replaceAll("()","o");
-    return value;
+    return  command.replaceAll("(al)", "al").replaceAll("()","o");
+    
 };
